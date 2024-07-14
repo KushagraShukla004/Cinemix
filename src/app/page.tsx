75d28927553d579f18ebed2aea4dc6ed608ab2b4
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-14 21:03:20 +0530

    Navbar Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { trpcClient } from '@/trpc/clients/client'
 import { trpcServer } from '@/trpc/clients/server'
-import { UserButton } from '@clerk/nextjs'
 
 export default async function Home() {
   const movies = await trpcServer.movies.movies.query()
   return (
     <main className="bg-slate-500 p-6">
-      Hello! <UserButton />
+      Hello!
       <div className="bg-primary-600 text-slate-100">
         {movies.map((movie) => (
           <div key={movie.id}>
---
+++ src/components/organisms/Navbar.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from '@clerk/nextjs'
+import { Brand } from '../molecules/Brand'
+import { Container } from '../atoms/container'
+import { ModeToggle } from '../molecules/ModeToggle'
+import { Sidebar } from './Sidebar'
+
+export interface INavbarProps {}
+
+export const Navbar = () => {
+  return (
+    <nav className="sticky top-0 w-full h-12 border-2 border-white border-y bg-white/40 backdrop-blur backdrop-filter">
+      <Container>
+        <div className="flex items-center justify-between">
+          <Brand />{' '}
+          <div className="flex items-center gap-2">
+            <ModeToggle />
+            <UserButton />
+            <Sidebar />
+          </div>
+        </div>
+      </Container>
+    </nav>
+  )
+}

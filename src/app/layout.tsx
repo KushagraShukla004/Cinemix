33f049dd3312ea70016af59797793431571f85d2
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-14 20:27:58 +0530

    Added Dark Mode Toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
+//Auth provider
 import { ClerkProvider } from '@clerk/nextjs'
 import { TRPCReactProvider } from '@/trpc/clients/client'
+
+//Components
 import { Toaster } from '@/components/molecules/Toaster/toaster'
+import { Container } from '@/components/atoms/container'
+import { Navbar } from '@/components/organisms/Navbar'
+import { ThemeProvider } from '@/util/styles/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,8 +29,16 @@ export default function RootLayout({
       <TRPCReactProvider>
         <html lang="en">
           <body className={inter.className}>
-            <main>{children}</main>
-            <Toaster />
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Navbar />
+              <Container>{children}</Container>
+              <Toaster />
+            </ThemeProvider>
           </body>
         </html>
       </TRPCReactProvider>

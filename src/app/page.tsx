a6a32ecbc95f64d75adc0c8ae78fd631e1d6d5fd
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-31 16:47:33 +0530

    Ticket generation with qr code generation (project complete)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-import { trpcClient } from '@/trpc/clients/client'
-import { trpcServer } from '@/trpc/clients/server'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Search } from 'lucide-react'
 
-export default async function Home() {
-  const movies = await trpcServer.movies.movies.query()
+export default function Page() {
   return (
-    <main className="bg-slate-500 p-6">
-      Hello!
-      <div className="bg-primary-600 text-slate-100">
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <div>{movie.id}</div>
-            <div>{movie.title}</div>
-            <div>{movie.director}</div>
+    <main>
+      <div className="flex flex-col gap-4 items-center mt-24">
+        <Image
+          alt=""
+          width={600}
+          height={600}
+          src="/default.png"
+          className="w-full max-w-2xl rounded-lg shadow-lg shadow-black/20 border-4 border-black aspect-square object-cover object-bottom "
+        />
+
+        <div className=" flex flex-col  items-center justify-center space-y-2 font-black  -m-40">
+          <div className="flex flex-col items-center p-12">
+            <div className="z-10 inline-block text-8xl text-slate-50">
+              Cinemix
+            </div>
+            <Link
+              href="/cinemas"
+              className="z-10 flex items-center gap-2 px-3 py-2 text-xl font-medium underline underline-offset-4"
+            >
+              <Search /> Search movies now
+            </Link>
           </div>
-        ))}
+        </div>
       </div>
     </main>
   )
---
+++ src/components/templates/Tickets.tsx
@@ -0,0 +1,147 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { getDownloadURL, ref } from 'firebase/storage'
+
+import { format } from 'date-fns'
+
+import { Button } from '../atoms/button'
+import { storage } from '@/util/config/firebase'
+import { useAuth } from '@clerk/nextjs'
+import { trpcClient } from '@/trpc/clients/client'
+import { Loading } from '../molecules/Loading'
+import { RouterOutputs } from '@/trpc/clients/types'
+import { SimpleDialog } from '../organisms/SimpleDialog'
+import { SeatNumber } from '../organisms/SearchUtils/SeatNumber'
+
+export interface ITicketsProps {}
+
+async function getImageUrl(path: string): Promise<string> {
+  // Create a reference to the file
+  const fileRef = ref(storage, path)
+
+  // Get the download URL
+  const url = await getDownloadURL(fileRef)
+
+  return url
+}
+
+export const TicketMovie = ({
+  ticket,
+}: {
+  ticket: RouterOutputs['tickets']['myTickets'][0]
+}) => {
+  const [open, setOpen] = useState(false)
+  const posterUrl = ticket.Bookings[0].Showtime.Movie.posterUrl || '/film.png'
+
+  return (
+    <div
+      key={ticket.id}
+      className="relative p-6 overflow-hidden rounded-lg"
+      style={{
+        backgroundImage: `url(${posterUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="relative z-10 p-4 bg-white/90 rounded-lg backdrop-filter backdrop-blur-md">
+        <SimpleDialog open={open} setOpen={setOpen} title={'QR Code'}>
+          <QRCode url={ticket.qrCode || ''} />
+        </SimpleDialog>
+        <div className="flex gap-6">
+          <Image
+            width={200}
+            height={200}
+            className="rounded"
+            src={posterUrl}
+            alt={ticket.Bookings[0].Showtime.Movie.title}
+          />
+          <div>
+            <div className="flex items-start justify-between gap-6">
+              <div>
+                <div className="text-lg font-semibold">
+                  {ticket.Bookings[0].Showtime.Movie.title}
+                </div>
+                <div>{ticket.Bookings[0].Showtime.Screen.Cinema.name}</div>
+              </div>
+              <div className="flex flex-col items-center p-1 mb-5 border rounded">
+                <div className="text-xs">Screen</div>
+                <div className="text-xl font-bold">
+                  {ticket.Bookings[0].Showtime.Screen.number}
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold">
+                {format(new Date(ticket.Bookings[0].Showtime.startTime), 'p')}
+              </div>
+              <div className="text-xs">
+                {format(new Date(ticket.Bookings[0].Showtime.startTime), 'PP')}
+              </div>
+            </div>
+            <div className="mt-4">
+              <div>Seats</div>
+              <div className="flex flex-wrap gap-2 ">
+                {ticket.Bookings.map((booking) => (
+                  <SeatNumber
+                    key={booking.id}
+                    column={booking.column}
+                    row={booking.row}
+                  />
+                ))}
+              </div>
+            </div>
+            <Button
+              variant="default"
+              className="mt-4"
+              size="sm"
+              onClick={() => setOpen(true)}
+            >
+              View QR code
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const Tickets = ({}: ITicketsProps) => {
+  const { userId } = useAuth()
+  const [skip, setSkip] = useState(0)
+  const [take, setTake] = useState(6)
+  const { data, isLoading } = trpcClient.tickets.myTickets.useQuery()
+
+  const [open, setOpen] = useState(false)
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <div className="flex flex-col">
+      {data?.map((ticket) => <TicketMovie key={ticket.id} ticket={ticket} />)}
+    </div>
+  )
+}
+
+export const QRCode = ({ url }: { url: string }) => {
+  const [picUrl, setPicUrl] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchImageUrl() {
+      setLoading(true)
+      const imageUrl = await getImageUrl(url)
+      setLoading(false)
+      setPicUrl(imageUrl)
+    }
+
+    fetchImageUrl()
+  }, [url])
+
+  if (loading) return <Loading />
+
+  return <Image width={200} height={200} src={picUrl || ''} alt={'ticket'} />
+}

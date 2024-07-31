f44afaf8ea38c1dd337beec32520754eb7582b2e
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-31 16:44:37 +0530

    Search Cinema Location using Map

---
+++ src/components/organisms/SearchPlace.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Loader } from 'lucide-react'
+import * as React from 'react'
+
+import { useMap } from 'react-map-gl'
+import { Input } from '../atoms/input'
+import { ViewState } from '../organisms/Map/Map'
+import { useSearchLocation } from '@/util/hooks'
+
+export function SearchPlace({
+  onLocationChange,
+}: {
+  onLocationChange?: (location: ViewState) => void
+}) {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState('')
+
+  const { loading, setSearchText, locationInfo } = useSearchLocation()
+
+  React.useEffect(() => {
+    setSearchText(value)
+  }, [setSearchText, value])
+
+  const { current: map } = useMap()
+
+  return (
+    <div className="relative">
+      <Input
+        placeholder="Search place..."
+        value={value}
+        onChange={(e) => {
+          setOpen(e.target.value.length > 0)
+          setValue(e.target.value)
+        }}
+      />
+      {open ? (
+        <div className="bg-background">
+          {loading && <Loader />}
+          <div className="absolute z-10 top-full bg-gray-300/90 rounded-lg">
+            {locationInfo.map((place) => (
+              <button
+                className="block w-full p-2 text-left underline underline-offset-4 hover:font-bold"
+                key={place.placeName}
+                onClick={() => {
+                  const {
+                    latLng: [latitude, longitude],
+                  } = place
+                  if (onLocationChange) {
+                    onLocationChange({ latitude, longitude, zoom: 10 })
+                  }
+                  setOpen(false)
+                  setValue('')
+                  map?.flyTo({
+                    center: { lat: latitude, lng: longitude },
+                    zoom: 10,
+                    essential: true,
+                  })
+                }}
+              >
+                {place.placeName}
+              </button>
+            ))}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  )
+}
--- src/trpc/server/routers/movies.ts
+++ src/trpc/server/routers/movies.ts
@@ -1,8 +1,13 @@
 import { schemaCreateMovie } from '@/forms/createMovie'
 import { createTRPCRouter, protectedProcedure, publicProcedure } from '..'
+import { findManyMovieArgsSchema } from './dtos/movies.input'
+import { z } from 'zod'
 
 export const moviesRouter = createTRPCRouter({
   movies: publicProcedure.query(({ ctx }) => {
+    return ctx.db.movie.findMany()
+  }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
     return ctx.db.movie.findMany()
   }),
   createMovie: protectedProcedure('admin')
@@ -10,6 +15,33 @@ export const moviesRouter = createTRPCRouter({
     .mutation(({ ctx, input }) => {
       return ctx.db.movie.create({
         data: input,
+      })
+    }),
+  moviesPerCinema: publicProcedure
+    // .input(findManyMovieArgsSchema)
+    .input(z.object({ cinemaId: z.number() }))
+    .query(({ input: { cinemaId }, ctx }) => {
+      // const { cursor, distinct, orderBy, skip, take, where, cinemaId } = input
+
+      return ctx.db.movie.findMany({
+        // cursor,
+        // distinct,
+        // orderBy,
+        // skip,
+        // take,
+        where: {
+          // ...where,
+          Showtimes: {
+            some: {
+              startTime: {
+                gt: new Date(),
+              },
+              Screen: {
+                cinemaId,
+              },
+            },
+          },
+        },
       })
     }),
 })

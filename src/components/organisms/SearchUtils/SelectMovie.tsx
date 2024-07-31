8f86903ae9dfdeb2e85e14cbfd64a5f026c4c9fe
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-31 16:45:42 +0530

    Select Movie and movie timings

---
+++ src/components/organisms/SearchUtils/SelectMovie.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { Loading } from '@/components/molecules/Loading'
+import { trpcClient } from '@/trpc/clients/client'
+import { useHandleSearch } from '@/util/hooks'
+import { CinemaSelectCard } from './CinemaSelectCard'
+
+export const SelectMovie = ({ cinemaId }: { cinemaId: number }) => {
+  const { data, isLoading } = trpcClient.movies.moviesPerCinema.useQuery({
+    cinemaId,
+  })
+
+  const { params, addParam, deleteParams } = useHandleSearch()
+
+  if (data?.length === 0) {
+    return <div>Currently no shows are running in this cinema.</div>
+  }
+
+  return (
+    <div>
+      <div>Select movie</div>
+      {isLoading ? <Loading /> : null}
+
+      <div className="grid grid-cols-3 gap-2">
+        {data?.map((movie) => (
+          <button
+            key={movie.id}
+            onClick={() => {
+              deleteParams(['showtimeId', 'screenId'])
+              addParam('movieId', movie.id)
+            }}
+          >
+            <CinemaSelectCard
+              movie={movie}
+              selected={params.get('movieId') === movie.id.toString()}
+            />
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}

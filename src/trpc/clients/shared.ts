4628268b182995082fa3da1d0ba05788d7814e29
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-09-22 15:58:10 +0530

    hosted url

--- src/trpc/clients/shared.ts
+++ src/trpc/clients/shared.ts
@@ -2,10 +2,10 @@
 
 function getBaseUrl() {
   if (typeof window !== 'undefined') return ''
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  if (process.env.RAILWAY_URL) return `https://${process.env.RAILWAY_URL}`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
-
+console.log('getBaseUrl(): ', getBaseUrl())
 export function getUrl() {
   return getBaseUrl() + '/api/trpc'
 }
